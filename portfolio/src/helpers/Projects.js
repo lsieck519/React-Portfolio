aa494be35502aa3fd0fdbb7909d2c1d8724a6b85
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-04-14 16:17:46 -0500

    organizing files and adding project data

---
+++ portfolio/src/helpers/Projects.js
@@ -0,0 +1,38 @@
+import TechBlog from "../assets/TechBlogImg.png"
+import WeatherDash from "../assets/WeatherDashImg.png"
+import TextEditor from "../assets/JATEImg.png"
+import Belle from "../assets/BelleImg.png"
+import RecipeMe from '../assets/RecipeMeImg.png';
+
+export const ProjectList = [
+  {
+    title: 'TechBlog',
+    image: TechBlog,
+    repo: 'https://github.com/lsieck519/Tech-Blog',
+    link: 'https://mighty-shore-51821.herokuapp.com/',
+  },
+  {
+    title: 'WeatherDash',
+    image: WeatherDash,
+    repo: 'https://github.com/lsieck519/Weather-Dash',
+    link: 'https://lsieck519.github.io/Weather-Dash/',
+  },
+  {
+    title: 'TextEditor',
+    image: TextEditor,
+    repo: 'https://github.com/lsieck519/Text-Editor',
+    link: 'https://safe-basin-28779.herokuapp.com/',
+  },
+  {
+    title: 'Belle',
+    image: Belle,
+    repo: 'https://github.com/lsieck519/Calendar-Dashboard',
+    link: 'https://lsieck519.github.io/Calendar-Dashboard/',
+  },
+  {
+    title: 'RecipeMe',
+    image: RecipeMe,
+    repo: 'https://github.com/lsieck519/RecipeMe',
+    link: 'https://quiet-forest-61314.herokuapp.com/',
+  },
+];
